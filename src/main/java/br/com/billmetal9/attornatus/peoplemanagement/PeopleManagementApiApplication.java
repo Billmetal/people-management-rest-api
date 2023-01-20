07bf2e5d95fb2f8a988affb7de6f6cf30cbc1775
Willian K <billmetal9@gmail.com>
2023-01-20 04:56:13 -0300

    Adicionado configuração CORS

--- src/main/java/br/com/billmetal9/attornatus/peoplemanagement/PeopleManagementApiApplication.java
+++ src/main/java/br/com/billmetal9/attornatus/peoplemanagement/PeopleManagementApiApplication.java
@@ -2,6 +2,9 @@ package br.com.billmetal9.attornatus.peoplemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Nome da Aplicação : Gerenciamento de Pessoas
@@ -19,6 +22,16 @@ public class PeopleManagementApiApplication {
 	// Método principal da aplicação
 	public static void main(String[] args) {
 		SpringApplication.run(PeopleManagementApiApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/").allowedOrigins("*");
+			}
+		};
 	}
 
 }
